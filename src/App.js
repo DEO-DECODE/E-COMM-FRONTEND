5a743641f0c6e1e5080253bcf89b0a1efc517bf1
Deo Raj <d.jrajsingh81@gmail.com>
2023-09-30 17:58:50 +0530

    Done with functionality of Private Component.

--- src/App.js
+++ src/App.js
@@ -3,22 +3,25 @@ import Nav from "./components/Nav";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Footer from "./components/Footer";
 import SignUp from "./components/SignUp";
-import "./App.css"
+import PrivateComponent from "./components/PrivateComponent.js";
+import "./App.css";
 const App = () => {
   return (
     <div className="App">
       <BrowserRouter>
         <Nav />
         <Routes>
-          <Route path="/" element={<h1>Product listing Component</h1>}/>
-          <Route path="/add" element={<h1>Add Product Component</h1>}/>
-          <Route path="/update" element={<h1>Update Product Component</h1>}/>
-          <Route path="/logout" element={<h1>Logout Component</h1>}/>
-          <Route path="/profile" element={<h1>Profile Component</h1>}/>
-          <Route path="/signup" element={<SignUp/>}/>
+          <Route element={<PrivateComponent />}>
+            <Route path="/" element={<h1>Product Component</h1>} />
+            <Route path="/add" element={<h1> AddProduct Component</h1>} />
+            <Route path="/update" element={<h1>Update Product Component</h1>} />
+            <Route path="/logout" element={<h1> Logout Component</h1>} />
+            <Route path="/profile" element={<h1>Profile Component</h1>} />
+          </Route>
+          <Route path="/signup" element={<SignUp />} />
         </Routes>
       </BrowserRouter>
-      <Footer/>
+      <Footer />
     </div>
   );
 };
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,12 +1,20 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+
 const SignUp = () => {
   const [name, setName] = useState("");
-  const [password, setPassword] = useState("");
   const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const navigate = useNavigate();
+  useEffect(() => {
+    const auth = localStorage.getItem("user");
+    if (auth) {
+      navigate("/");
+    }
+  }, []);
+
   const collectData = async () => {
-    console.log(name, email, password);
+    console.warn(name, email, password);
     let result = await fetch("http://localhost:5000/register", {
       method: "post",
       body: JSON.stringify({ name, email, password }),
@@ -16,45 +24,38 @@ const SignUp = () => {
     });
     result = await result.json();
     console.warn(result);
-    if (result) {
-      navigate("/");
-    }
+    localStorage.setItem("user", JSON.stringify(result));
+    navigate("/");
   };
+
   return (
-    <div>
+    <div className="register">
       <h1>Register</h1>
       <input
         className="inputBox"
         type="text"
         placeholder="Enter Name"
-        onChange={(e) => {
-          setName(e.target.value);
-        }}
         value={name}
+        onChange={(e) => setName(e.target.value)}
       />
       <input
         className="inputBox"
         type="text"
         placeholder="Enter Email"
-        onChange={(e) => {
-          setEmail(e.target.value);
-        }}
         value={email}
+        onChange={(e) => setEmail(e.target.value)}
       />
       <input
         className="inputBox"
         type="password"
-        placeholder="Enter Password"
-        onChange={(e) => {
-          setPassword(e.target.value);
-        }}
+        placeholder="Enter password"
         value={password}
+        onChange={(e) => setPassword(e.target.value)}
       />
-      <button className="appButton" type="button" onClick={collectData}>
+      <button onClick={collectData} className="appButton" type="button">
         Sign Up
       </button>
     </div>
   );
 };
-
 export default SignUp;
