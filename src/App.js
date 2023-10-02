8bdf76ef7be19395044872ba2ee17536f1c15eeb
Deo Raj <d.jrajsingh81@gmail.com>
2023-10-02 18:34:52 +0530

    Login UI

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from "react";
+import "./App.css";
 import Nav from "./components/Nav";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Footer from "./components/Footer";
 import SignUp from "./components/SignUp";
-import PrivateComponent from "./components/PrivateComponent.js";
-import "./App.css";
-const App = () => {
+import PrivateComponent from "./components/PrivateComponent";
+import Login from "./components/Login";
+function App() {
   return (
     <div className="App">
       <BrowserRouter>
@@ -18,12 +18,14 @@ const App = () => {
             <Route path="/logout" element={<h1> Logout Component</h1>} />
             <Route path="/profile" element={<h1>Profile Component</h1>} />
           </Route>
+
           <Route path="/signup" element={<SignUp />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </BrowserRouter>
       <Footer />
     </div>
   );
-};
+}
 
 export default App;
